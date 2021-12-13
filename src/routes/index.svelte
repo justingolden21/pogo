3662623c5982a7f4e1538c842a8f5ac06a65aae4
Justin Golden <justingolden21@gmail.com>
2021-12-12 16:20:52 -0800

    add img (from home)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,23 @@
 <script>
 	import pokedex from '../data/pokedex.json';
 
-	let id = 0;
-	$: pokemon = pokedex[id];
+	let id = 1;
+	$: pokemon = pokedex.filter((p) => p.dex === id)[0];
 </script>
 
 <svelte:head>
 	<title>{pokemon.name}</title>
 </svelte:head>
 
+<img
+	src={`https://img.pokemondb.net/sprites/home/normal/${pokemon.name
+		.replace(' ', '-')
+		.replace('.', '')
+		.toLowerCase()}.png`}
+	alt={pokemon.name}
+/>
+
 <p class="mt-4 text-left">#{id}</p>
 <h1>{pokemon.name}</h1>
-<button on:click={() => (id = id + 1)}>Next</button>
+<button on:click={() => id--}>Prev</button>
+<button on:click={() => id++}>Next</button>
